479affc1ab84f64e70b63154f219eb99e0b720e3
Chandrashekhar Jha <jhachandrashekhar163@gmail.com>
2026-07-20 22:43:40 +0530

    Third Question completed

---
+++ Arrays/Check_if_the_array_is_sorted.js
@@ -0,0 +1,18 @@
+// Problem Statement: Given an array of size n, write a program to check if the given array is sorted in (ascending / Increasing / Non-decreasing) order or not. If the array is sorted then return True, Else return False.
+
+class Solution {
+  SortedArrays(nums) {
+    const n = nums.length;
+
+    if (n == 0 || n == 1) {
+      return true;
+    }
+
+    for (let i = 0; i <= n-1; i++) {
+      if (nums[i] < nums[i + 1]) {
+        return false;
+      }
+    }
+    return false
+  }
+}
