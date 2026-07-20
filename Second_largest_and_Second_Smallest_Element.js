05d7a748be7997d7446d0797c1149dc6837bbcc6
Chandrashekhar Jha <jhachandrashekhar163@gmail.com>
2026-07-20 15:31:53 +0530

    Second question of the day

--- Second_largest_and_Second_Smallest_Element.js
+++ Second_largest_and_Second_Smallest_Element.js
@@ -1,2 +1,26 @@
 //Given an array, find the second smallest and second largest element in the array. Print ‘-1’ in the event that either of them doesn’t exist.
 
+class Solution {
+    secondLargestElement(nums) {
+
+        let n = nums.length;
+
+        if (n < 2) {
+            return -1;
+        }
+
+        nums.sort((a, b) => a - b);
+
+        let largest = nums[n - 1];
+
+        for (let i = n - 2; i >= 0; i--) {
+
+            if (nums[i] != largest) {
+                return nums[i];
+            }
+
+        }
+
+        return -1;
+    }
+}
