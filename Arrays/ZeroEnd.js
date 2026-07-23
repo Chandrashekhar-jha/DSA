49a69e413aaeca9588405cc1c081f8e77c904ba8
Chandrashekhar Jha <jhachandrashekhar163@gmail.com>
2026-07-24 00:02:01 +0530

    Optimal method

--- Arrays/ZeroEnd.js
+++ Arrays/ZeroEnd.js
@@ -1,5 +1,6 @@
+// brute Method
 class Solution {
-    zeroEndArray(nums){
+    moveZeroes(nums){
         let n = nums.length
 
         let temp = []
@@ -13,5 +14,42 @@ class Solution {
         for(let i = 0 ; i<temp.length; i++){
             nums[i]=temp[i]
         }
+    }
+}
+ // Optimal method
+
+ class Rajan {
+    moveZeroes(nums) {
+
+        let n = nums.length;
+
+        // Step 1: Find first zero
+        let j = -1;
+
+        for (let i = 0; i < n; i++) {
+            if (nums[i] === 0) {
+                j = i;
+                break;
+            }
+        }
+
+        // No zero found
+        if (j === -1) {
+            return nums;
+        }
+
+        // Step 2: Move non-zero elements forward
+        for (let i = j + 1; i < n; i++) {
+
+            if (nums[i] !== 0) {
+
+                // JavaScript swap
+                [nums[j], nums[i]] = [nums[i], nums[j]];
+
+                j++;
+            }
+        }
+
+        return nums;
     }
 }
