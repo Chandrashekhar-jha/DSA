666d2d27a4abc9f8b6c12e37c84bd556e4e752c1
Chandrashekhar Jha <jhachandrashekhar163@gmail.com>
2026-07-28 23:38:49 +0530

    Optimal method for sorted array

--- Arrays/Check_if_the_array_is_sorted.js
+++ Arrays/Check_if_the_array_is_sorted.js
@@ -16,3 +16,16 @@ class Solution {
     return true
   }
 }
+
+// Optimal method
+
+class Solution {
+    // Function to check if the array is sorted
+    isSorted(arr, n) {
+        for (let i = 1; i < n; i++) {
+            if (arr[i] < arr[i - 1])  // If any element is smaller than the previous one, return false
+                return false;
+        }
+        return true;  // Return true if the array is sorted
+    }
+}
